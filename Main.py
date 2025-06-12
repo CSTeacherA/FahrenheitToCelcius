6934576b0140c8597660cd09345dffabcb5f89c6
CSTeacherA <awillette@thewoodwardschool.org>
2025-06-12 19:28:29 -0400

    Create Main.py
    Add formulas and instructions

---
+++ Main.py
@@ -0,0 +1,9 @@
+# write a program that will accept degrees in fahrenheit as an input from a user and convert it to celcius 
+# use the functions int(input()) to get an input and convert it to a number
+
+# the formula to convert fahrenheit to celcius is C = (f - 32 ) * (5/9)
+
+
+# use print() to display the converted degrees in celcius
+
+
